2629e2f56ef4d7d5a141fa77b4c50b6f13020c9a
Hardware-Software <ashu2100ag@gmail.com>
2024-12-03 12:24:09 +0530

    Time: 25 ms (25.90%) | Memory: 86.7 MB (11.32%) - LeetSync

--- December2024Badge/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
+++ December2024Badge/2232-adding-spaces-to-a-string/adding-spaces-to-a-string.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
    string addSpaces(string s, vector<int>& spaces) {
        int n = spaces.size();
-        ostringstream newString;
+        string newString="";
         int stringsize = s.size();
         int i=0;
         int j=0;
         while(j<n)
         {
-            newString<<s.substr(i,spaces[j]-i)<<" ";
+            newString+=s.substr(i,spaces[j]-i)+" ";
             i = spaces[j];
             j++;
         }
-        newString<<s.substr(i);
-        return newString.str();
+        newString+=s.substr(i,stringsize);
+        return newString;
     }
 };
