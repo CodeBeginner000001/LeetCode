994dc3e7af0392d56e6a71030f778b7833daf81f
Hardware-Software <ashu2100ag@gmail.com>
2024-12-04 12:35:30 +0530

    Time: 8 ms (26.52%) | Memory: 17.2 MB (7.53%) - LeetSync

--- December2024Badge/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ December2024Badge/3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
-    if (str1.size() < str2.size())
+        int n = str1.size();
+    if (n< str2.size())
         return false;
     int i = 0;
     int j = 0;
-    while (i < str1.size() && j < str2.size())
+    while (i < n && j < str2.size())
     {
         if ((str1[i] == 'z' && str2[j] == 'a') || 
             (str1[i] == str2[j]) || 
